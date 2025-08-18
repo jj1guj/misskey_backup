06e1213fdb0c64c18a059e003ed8766146cf961f
jj1guj <jj1guj@jj1guj.net>
2025-08-18 16:26:25 +0900

    add logging message

--- backup_all.sh
+++ backup_all.sh
@@ -14,26 +14,33 @@ mkdir -p $backup_directory
 cd $misskey_directory_path
 
 # backup ai
+echo "Backing up AI memory..."
 cp $ai_memory_path $backup_directory/ai_memory_${filename_date}.json
 
 #  backup db
+echo "Backing up database..."
 sudo docker compose exec -T db bash -c "pg_dump -Fc -U example-misskey-user -d misskey > /var/lib/postgresql/data/misskey_db.dump"
 filepath=$misskey_directory_path/misskey_db_${filename_date// /_}.dump
 sudo mv $misskey_directory_path/db/misskey_db.dump $backup_directory/$filepath
 
 # backup redis
+echo "Backing up Redis data..."
 sudo cp $misskey_directory_path/redis/dump.rdb $backup_directory/dump_${filename_date}.rdb
 
 # backup files
+echo "Backing up files..."
 sudo zip -r $backup_directory/$files_path.zip $files_path
 
 # zip all backups
+echo "Creating zip archive of all backups..."
 sudo zip -r $backup_directory/misskey_backup_${filename_date}.zip $backup_directory
 
 # upload to gdrive
+echo "Uploading backups to Google Drive..."
 sudo gdrive files upload --parent $gdrive_folder_id $backup_directory/misskey_backup_${filename_date}.zip
 
 # clean up
+echo "Cleaning up local backup files..."
 sudo rm -rf $backup_directory
 sudo rm -rf $backup_directory/misskey_backup_${filename_date}.zip
 
