256375b39c8d4cb3044990980bcaadd0ba4a6ddc
jj1guj <jj1guj@jj1guj.net>
2026-05-24 23:22:58 +0900

    GPG暗号化を追加

--- backup_all.sh
+++ backup_all.sh
@@ -4,10 +4,13 @@ misskey_server_url=$(cat config.json | jq -r '.misskey_server_url')
 misskey_api_key=$(cat config.json | jq -r '.misskey_api_key')
 misskey_directory_path=$(cat config.json | jq -r '.misskey_directory_path')
 ai_memory_path=$(cat config.json | jq -r '.ai_memory_path')
+gpg_password=$(cat config.json | jq -r '.gpg_password')
 
 d=$(date)
 filename_date=${d// /_}
 backup_directory=/tmp/misskey_backup_$filename_date
+backup_zip=/tmp/misskey_backup_${filename_date}.zip
+backup_gpg=/tmp/misskey_backup_${filename_date}.zip.gpg
 mkdir -p $backup_directory
 
 cd $misskey_directory_path
@@ -32,16 +35,21 @@ sudo zip -q -r $backup_directory/files_${filename_date}.zip $misskey_directory_p
 
 # zip all backups
 echo "Creating zip archive of all backups..."
-sudo zip -q -r $backup_directory/misskey_backup_${filename_date}.zip $backup_directory
+sudo zip -q -r $backup_zip $backup_directory
+
+# encrypt archive with gpg
+echo "Encrypting backup archive with GPG..."
+sudo gpg --batch --yes --pinentry-mode loopback --passphrase "$gpg_password" --symmetric --cipher-algo AES256 --output $backup_gpg $backup_zip
 
 # upload to gdrive
 echo "Uploading backups to Google Drive..."
-sudo gdrive files upload --parent $gdrive_folder_id $backup_directory/misskey_backup_${filename_date}.zip
+sudo gdrive files upload --parent $gdrive_folder_id $backup_gpg
 
 # clean up
 echo "Cleaning up local backup files..."
 sudo rm -rf $backup_directory
-sudo rm -rf $backup_directory/misskey_backup_${filename_date}.zip
+sudo rm -f $backup_zip
+sudo rm -f $backup_gpg
 
 # notify completion
 curl -XPOST -H 'Content-Type:application/json' -d "{\"i\":\"$misskey_api_key\",\"text\":\"$(date)\nBackup completed\"}" $misskey_server_url/api/notes/create
