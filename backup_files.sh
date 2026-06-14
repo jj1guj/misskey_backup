b86ca646982a53097555d38b8d7e3890b1da71f3
jj1guj <jj1guj@jj1guj.net>
2026-06-14 15:20:04 +0900

    バックアップ処理を改善
    * filesのバックアップはrcloneによる増分差分のみバックアップするように
    * filesとそれ以外でサービスを分離

--- backup_files.sh
+++ backup_files.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-gdrive_folder_id=$(cat config.json | jq -r '.gdrive_folder_id')
 misskey_server_url=$(cat config.json | jq -r '.misskey_server_url')
 misskey_api_key=$(cat config.json | jq -r '.misskey_api_key')
-files_path=$(cat config.json | jq -r '.files_path')
-sudo zip -r $files_path.zip $files_path
-sudo gdrive files upload --parent $gdrive_folder_id $files_path.zip
-sudo rm -rf $files_path.zip
+misskey_directory_path=$(cat config.json | jq -r '.misskey_directory_path')
+rclone_config_path=$(cat config.json | jq -r '.rclone_config_path')
+
+echo "Syncing files to Google Drive..."
+sudo rclone sync $misskey_directory_path/files gdrive-crypt: --config $rclone_config_path --transfers 4
+
+# notify completion
 curl -XPOST -H 'Content-Type:application/json' -d "{\"i\":\"$misskey_api_key\",\"text\":\"$(date)\nFiles backup completed\"}" $misskey_server_url/api/notes/create
